8a590f62faceb944ea80563cfe2623e98da326bb
k906506 <k906506@naver.com>
2021-11-09 13:35:09 +0900

    Week8 - Gaussian & Backward & RANSAC

---
+++ venv/Week8/[CG]201701971_고도현_8주차_과제.py
@@ -0,0 +1,263 @@
+import numpy as np
+import cv2
+import random
+
+
+def my_padding(src):
+    (h, w, c) = src.shape
+    h_pad = 2
+    w_pad = 2
+    padding_img = np.zeros((h + h_pad * 2, w + w_pad * 2, 3))
+    padding_img[h_pad:h + h_pad, w_pad:w + w_pad, :] = src
+
+    # repetition padding
+    # up
+    padding_img[:h_pad, w_pad:w_pad + w, :] = src[0, :, :]
+    # down
+    padding_img[h_pad + h:, w_pad:w_pad + w, :] = src[h - 1, :, :]
+    # left
+    padding_img[:, :w_pad, :] = padding_img[:, w_pad:w_pad + 1, :]
+    # right
+    padding_img[:, w_pad + w:, :] = padding_img[:, w_pad + w - 1:w_pad + w, :]
+
+    return padding_img
+
+
+def my_get_Gaussian_filter(fshape, sigma=1):
+    (f_h, f_w) = fshape
+    y, x = np.mgrid[-(f_h // 2):(f_h // 2) + 1, -(f_w // 2):(f_w // 2) + 1]
+    # 2차 gaussian mask 생성
+    filter_gaus = 1 / (2 * np.pi * sigma ** 2) * np.exp(-((x ** 2 + y ** 2) / (2 * sigma ** 2)))
+    # mask의 총 합 = 1
+    filter_gaus /= np.sum(filter_gaus)
+    return filter_gaus
+
+
+def GaussianFiltering(src, fshape=(5, 5), sigma=1):
+    gaus = my_get_Gaussian_filter(fshape, sigma)  # 5 * 5 gaussian mask 생성
+    dst = src * gaus
+    return dst
+
+
+# bilinear interpolation
+def my_bilinear(img, x, y):
+    '''
+    :param img: 값을 찾을 img
+    :param x: interpolation 할 x좌표
+    :param y: interpolation 할 y좌표
+    :return: img[x,y]에서의 value (bilinear interpolation으로 구해진)
+    '''
+
+    bin_value = [[] for _ in range(5)]
+    index = 0
+
+    for i in range(-2, 3):
+        for j in range(-2, 3):
+            floorX, floorY = int(x - i), int(y - j)
+
+            t, s = (x - i) - floorX, (y - j) - floorY
+
+            zz = (1 - t) * (1 - s)
+            zo = t * (1 - s)
+            oz = (1 - t) * s
+            oo = t * s
+
+            interVal = img[floorY, floorX, :] * zz + img[floorY, floorX + 1, :] * zo + \
+                       img[floorY + 1, floorX, :] * oz + img[floorY + 1, floorX + 1, :] * oo
+
+            bin_value[index].append(interVal)
+
+        index += 1
+
+    gaussian_interVal = GaussianFiltering(bin_value)
+
+    return interVal
+
+
+def backward(img1, M):
+    '''
+    :param img1: 변환시킬 이미지
+    :param M: 변환 matrix
+    :return: 변환된 이미지
+    '''
+    h, w, c = img1.shape
+    result = np.zeros((h, w, c))
+
+    for row in range(h):
+        for col in range(w):
+            xy = (np.linalg.inv(M)).dot(np.array([[col, row, 1]]).T)
+
+            x_ = xy[0, 0]
+            y_ = xy[1, 0]
+
+            if x_ < 0 or y_ < 0 or (x_ + 1) >= w or (y_ + 1) >= h:
+                continue
+
+            result[row, col, :] = my_bilinear(img1, x_, y_)
+
+    return result
+
+
+def my_ls(matches, kp1, kp2):
+    '''
+    :param matches: keypoint matching 정보
+    :param kp1: keypoint 정보.
+    :param kp2: keypoint 정보2.
+    :return: X : 위의 정보를 바탕으로 Least square 방식으로 구해진 Affine
+                변환 matrix의 요소 [a, b, c, d, e, f].T
+    '''
+    A = []
+    B = []
+    for idx, match in enumerate(matches):
+        trainInd = match.trainIdx
+        queryInd = match.queryIdx
+
+        x, y = kp1[queryInd].pt
+        x_prime, y_prime = kp2[trainInd].pt
+
+        A.append([x, y, 1, 0, 0, 0])
+        A.append([0, 0, 0, x, y, 1])
+        B.append([x_prime])
+        B.append([y_prime])
+
+    A = np.array(A)
+    B = np.array(B)
+
+    try:
+        ATA = np.dot(A.T, A)
+        ATB = np.dot(A.T, B)
+        X = np.dot(np.linalg.inv(ATA), ATB)
+    except:
+        print('can\'t calculate np.linalg.inv((np.dot(A.T, A)) !!!!!')
+        X = None
+    return X
+
+
+def get_matching_keypoints(img1, img2, keypoint_num):
+    '''
+    :param img1: 변환시킬 이미지
+    :param img2: 변환 목표 이미지
+    :param keypoint_num: 추출한 keypoint의 수
+    :return: img1의 특징점인 kp1, img2의 특징점인 kp2, 두 특징점의 매칭 결과
+    '''
+    sift = cv2.SIFT_create(keypoint_num)
+
+    kp1, des1 = sift.detectAndCompute(img1, None)
+    kp2, des2 = sift.detectAndCompute(img2, None)
+
+    bf = cv2.BFMatcher(cv2.DIST_L2)
+
+    matches = bf.match(des1, des2)
+    matches = sorted(matches, key=lambda x: x.distance)
+
+    """
+    matches: List[cv2.DMatch]
+    cv2.DMatch의 배열로 구성
+
+    matches[i]는 distance, imgIdx, queryIdx, trainIdx로 구성됨
+    trainIdx: 매칭된 img1에 해당하는 index
+    queryIdx: 매칭된 img2에 해당하는 index
+
+    kp1[queryIdx]와 kp2[trainIdx]는 매칭된 점
+    """
+    return kp1, kp2, matches
+
+
+def feature_matching(img1, img2, keypoint_num=None):
+    kp1, kp2, matches = get_matching_keypoints(img1, img2, keypoint_num)
+
+    X = my_ls(matches, kp1, kp2)
+
+    M = np.array([[X[0][0], X[1][0], X[2][0]],
+                  [X[3][0], X[4][0], X[5][0]],
+                  [0, 0, 1]])
+
+    result = backward(img1, M)
+
+    return result.astype(np.uint8)
+
+
+def feature_matching_RANSAC(img1, img2, keypoint_num=None, iter_num=500, threshold_distance=10):
+    '''
+    :param img1: 변환시킬 이미지
+    :param img2: 변환 목표 이미지
+    :param keypoint_num: sift에서 추출할 keypoint의 수
+    :param iter_num: RANSAC 반복횟수
+    :param threshold_distance: RANSAC에서 inlier을 정할때의 거리 값
+    :return: RANSAC을 이용하여 변환 된 결과
+    '''
+    kp1, kp2, matches = get_matching_keypoints(img1, img2, keypoint_num)
+
+    matches_shuffle = matches.copy()
+
+    inliers = []  # 랜덤하게 고른 n개의 point로 구한 inlier개수 결과를 저장
+    M_list = []  # 랜덤하게 고른 n개의 point로 만든 affine matrix를 저장
+
+    for i in range(iter_num):
+        print('\rcalculate RANSAC ... %d ' % (int((i + 1) / iter_num * 100)) + '%', end='\t')
+        #######################################################################
+        # ToDo
+        # RANSAC을 이용하여 최적의 affine matrix를 찾고 변환하기
+        # 1. 랜덤하게 3개의 matches point를 뽑아냄
+        # 2. 1에서 뽑은 matches를 이용하여 affine matrix M을 구함
+        # 3. 2에서 구한 M을 모든 matches point와 연산하여 inlier의 개수를 파악
+        # 4. iter_num 반복하여 가장 많은 inlier를 가지는 M을 최종 affine matrix로 채택
+        ########################################################################
+        random.shuffle(matches_shuffle)  # 무작위로 섞고
+        three_points = matches_shuffle[:3]  # 맨 처음부터 3개 선택하여 matches point로 지정
+
+        M = my_ls(three_points, kp1, kp2)  # affine matrix M 계산
+
+        M = np.array([[M[0][0], M[1][0], M[2][0]],  # 3 by 3 을 맞춰주기 위해 [0, 0, 1] 추가
+                      [M[3][0], M[4][0], M[5][0]],
+                      [0, 0, 1]])
+
+        inlier_cnt = 0  # inlier의 개수를 세기 위한 변수
+
+        for idx, match in enumerate(matches):
+            trainInd = match.trainIdx
+            queryInd = match.queryIdx
+
+            x, y = kp1[queryInd].pt
+            x_, y_ = kp2[trainInd].pt
+
+            xy = np.array([[x, y, 1]]).T
+            xy_prime = np.dot(M, xy)
+
+            dist = L2_distance(x_, xy_prime[0][0], y_, xy_prime[1][0])
+
+            if dist < threshold_distance:  # inlier의 개수 파악
+                inlier_cnt += 1
+
+        inliers.append(inlier_cnt)  # inlier의 개수와 M을 저장
+        M_list.append(M)
+
+    print("max inliers : %d" % max(inliers))
+    best_M = M_list[inliers.index(max(inliers))]  # 가장 많은 inlier를 가지는 M을 최종 affine matrix로 채택
+
+    result = backward(img1, best_M)
+    return result.astype(np.uint8)
+
+
+def L2_distance(x1, x2, y1, y2):
+    return np.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+
+
+def main():
+    src = cv2.imread('Lena.png')
+    src = cv2.resize(src, None, fx=0.5, fy=0.5)
+    src = my_padding(src)
+    src2 = cv2.imread('LenaFaceShear.png')
+
+    result_RANSAC = feature_matching_RANSAC(src, src2)
+    result_LS = feature_matching(src, src2)
+    cv2.imshow('input', src)
+    cv2.imshow('result RANSAC 201701971', result_RANSAC)
+    cv2.imshow('result LS 201701971', result_LS)
+    cv2.imshow('goal', src2)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
